34a332612b8ff482b4affea9915f8a54dc236221
Yannick Tivisse <yti@odoo.com>
2018-09-12 17:01:51 +0200

    [IMP] plant_nursery: introduce the rating.mixin

--- plant_nursery/__manifest__.py
+++ plant_nursery/__manifest__.py
@@ -6,7 +6,7 @@
     'version': '1.0',
     'category': 'Tools',
     'summary': 'Plants and Customers Management',
-    'depends': ['web', 'mail'],
+    'depends': ['web', 'mail', 'rating'],
     'data': [
         'security/ir.model.access.csv',
         'views/assets.xml',
@@ -14,7 +14,9 @@
         'views/nursery_plant_views.xml',
         'views/nursery_customer_views.xml',
         'views/nursery_order_views.xml',
+        'views/rating_rating_views.xml',
         'data/ir_sequence_data.xml',
+        'data/mail_template_data.xml',
     ],
     'demo': [
     ],
